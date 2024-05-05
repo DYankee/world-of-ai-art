9ce82782ac2af017d059fd2924b199ecabb76d77
zachary geary <zpgeary@gmail.com>
2024-05-05 17:55:06 -0400

    added docker file for deployment

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM golang:1.22
+
+WORKDIR /app
+COPY go.mod go.sum ./
+
+RUN go mod download
+
+COPY ./ /app
+
+RUN go build main.go
+
+CMD /app/main
+
+EXPOSE 8080
--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import "github.com/labstack/echo/v4"
 func main() {
 	e := echo.New()
 	e.File("/", "index.html")
-
 	e.Static("/", "./")
-	e.Logger.Fatal(e.Start(":8081"))
+	e.Logger.Fatal(e.Start(":8080"))
 }
